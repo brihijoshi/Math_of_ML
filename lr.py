cd2b09a7ed542bffb57d0165122c74a3d52703c8
adsrc <aditya16217@iiitd.ac.in>
2018-01-04 21:46:10 +0530

    Completed regression code

--- lr.py
+++ lr.py
@@ -2,7 +2,7 @@ import random as rd
 import numpy as np
 import matplotlib.pyplot as plt
 from sympy import *
-from scipy import misc
+from scipy.misc import derivative
 
 fh = open("data/data_lr.csv","r")
 
@@ -19,16 +19,14 @@ plt.plot(x[1:],y[1:],'o')
 num_samples = len(x)
 #y=1+bx
 
-param_0 = 1
-param_1 = round(rd.uniform(4,40),2)
+#param_0 = 1
+#param_1 = round(rd.uniform(4,40),2)
 
-def cost_i(num, x, y):
-    return ( 1 / ( 2 * num_samples ) ) * ( ( THETA_0 + (theta_1 * x_i) - y_i ) ** 2 )
+def cost_i(num, param_0, param_1, x, y):
+    return ( 1 / ( 2 * num ) ) * ( ( param_0 + (param_1 * x) - y ) ** 2 )
 
 num_iters = 1
-THETA_0, theta_1, x_i, y_i = symbols('THETA_0 theta_1 x_i y_i', float = True)
-cost_i = ( 1 / ( 2 * num_samples ) ) * ( ( THETA_0 + (theta_1 * x_i) - y_i ) ** 2 )
-deriv = Derivative(cost_i, theta_1)
+
 
 alpha = 0.01
 
@@ -36,7 +34,8 @@ for i in range(num_iters):
     change = 0
 
     for k in range (1,num_samples):
-        change+= deriv.doit().subs({THETA_0 : param_0, theta_1 : param_1, x_i : x[k], y_i : y[k] })
+        change+= derivative(cost_i)
+        #change+= deriv.doit().subs({THETA_0 : param_0, theta_1 : param_1, x_i : x[k], y_i : y[k] })
     param_1 = param_1 - (alpha * change)
 
 
