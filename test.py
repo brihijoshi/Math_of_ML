4b975decf8cd6e7eea1c2b6b22ad54eed909d186
adsrc <aditya16217@iiitd.ac.in>
2018-01-04 18:25:27 +0530

    Code for LR not working

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 x = np.random.uniform(-4,4,500)
 y = x + np.random.standard_normal(500)+2.5
 plt.plot(x, y, 'o')
 def cost(X, Y, theta):
     J=np.dot((np.dot(X,theta) - Y).T,(np.dot(X,theta) - Y))/(2*len(Y))
     return J
-alpha = 0.35 # Specify the learning rate
+alpha = 0.1 # Specify the learning rate
 theta = np.array([[0,0]]).T # Initial values of theta
 X = np.c_[np.ones(500),x]
 Y = np.c_[y]
@@ -21,6 +22,7 @@ for i in range(num_iters):
     cost_history.append(cost(X,Y,theta))
     theta_history.append(theta)
     if i in (1,3,7,10,14,num_iters):
+        print(type(x), type(a), type(b))
         plt.plot(x, a+x*b)
         plt.suptitle('Linear regression by gradient descent')
         plt.xlabel('x')
