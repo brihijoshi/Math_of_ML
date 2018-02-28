7e7f45542f5b39e01ffc4ca4bc1beb5fc66fcf39
adsrc <aditya16217@iiitd.ac.in>
2018-02-28 13:48:49 +0530

    Cleaned the repository

--- tedit.py
+++
@@ -1,7 +0,0 @@
-fr = open('data/data_lr.csv','r')
-fw = open('data/lr.csv', 'a')
-
-for line in fr:
-    tokens = line.split(",")
-    fw.write(str(float(tokens[0].strip())/10)+','+str(float(tokens[1].strip())/10)+'\n')
-
